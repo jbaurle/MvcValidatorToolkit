5573eeb35c577548e922f91b90c85c30d38c19ec
bszafko_cp <SND\bszafko_cp>
2008-04-01 16:33:15 +0000

    added new sample to the website - using strongly typed ViewData with toolkit

--- MvcValidatorToolkit.SampleSite/Code/Sample3ValidationSet.cs
+++ MvcValidatorToolkit.SampleSite/Code/Sample3ValidationSet.cs
@@ -3,33 +3,7 @@ using System.Web.Mvc;
 
 namespace MvcValidatorToolkit.SampleSite
 {
-	public class Sample3ValidationSet : ValidationSet
+	public class Sample3ValidationSet : Sample2ValidationSet
 	{
-		string Username = "";
-		string Password = "";
-
-		protected override ValidatorCollection GetValidators()
-		{
-			return new ValidatorCollection
-			(
-				//new ValidateDate("startDate")
-				//new ValidateDate("startDate", "yyyy-mm-dd,yyyymmdd")
-				//new ValidateDate("startDate") { DateFormats = "yyyy-mm-dd,yyyymmdd" }
-				//new ValidateBuga("username"),
-				//new ValidateScriptMethod("username") { MethodName = "validateUsername" }
-				//new ValidateScriptMethod("username") { MethodName = "validateUsername", Parameters = "{ABC:2, XYZ:6}" }
-			);
-		}
-
-		protected bool ValidateUsername()
-		{
-			return true;
-		}
-
-		protected override void OnValidate()
-		{
-			if(Username.StartsWith("billy") && Password.StartsWith("gat"))
-				throw new ValidatorException("username", "The username/password combination is not valid");
-		}
 	}
 }
---
+++ MvcValidatorToolkit.SampleSite/Views/Sample/Sample3.aspx.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MvcValidatorToolkit.SampleSite.Sample
+{
+	[ValidationSet(typeof(Sample3ValidationSet))]
+	public partial class Sample3 : ViewPage<Sample3ViewData>
+	{
+	}
+}

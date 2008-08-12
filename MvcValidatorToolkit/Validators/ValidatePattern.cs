70333270188c444b234d8732cdbabba2e38f2ac1
jbaurle_cp <SND\jbaurle_cp>
2008-08-12 08:29:02 +0000

    Updated validators; ValidatePattern can now be used multiple on a page.

--- MvcValidatorToolkit/Validators/ValidatePattern.cs
+++ MvcValidatorToolkit/Validators/ValidatePattern.cs
@@ -5,96 +5,85 @@ using System.Text.RegularExpressions;
 
 namespace System.Web.Mvc
 {
-    /// <summary>
-    /// Regular expression validator
-    /// Author: djsneed31
-    /// Modifications: Bartek Szafko
-    /// </summary>
-    public class ValidatePattern : Validator
-    {
-        /// <summary>
-        /// Regular expression that is tested on client
-        /// </summary>
-        public string Pattern { get; set; }
+	/// <summary>
+	/// Represents a validator which validates the defined element list against a given regular expression 
+	/// pattern.
+	/// </summary>
+	public class ValidatePattern : Validator
+	{
+		public string Pattern { get; set; }
+		public string ScriptPattern { get; set; }
 
-        /// <summary>
-        /// Regular expression that is tested on client
-        /// </summary>
-        public string ScriptPattern { get; set; }
+		/// <summary>
+		/// Initializes a new instance of the ValidateDate class with the given elements to validate.
+		/// </summary>
+		public ValidatePattern(string elementsToValidate)
+			: base(elementsToValidate)
+		{ }
 
-        /// <summary>
-        /// Initializes a new instance of the ValidateDate class with the given elements to validate.
-        /// </summary>
-        public ValidatePattern(string elementsToValidate)
-            : base(elementsToValidate)
-        {}
+		/// <summary>
+		/// Gets a ValidatorMethodData instance that defines the client-side validator used by the
+		/// jQuery validation plugin.
+		/// </summary>
+		public override ValidatorMethodData GetClientMethodData()
+		{
+			string scriptPattern = ScriptPattern;
 
-        /// <summary>
-        /// Gets a ValidatorMethodData instance that defines the client-side validator used by the
-        /// jQuery validation plugin.
-        /// </summary>
-        public override ValidatorMethodData GetClientMethodData()
-        {
-            string scriptPattern = this.ScriptPattern;
-            // if only server side pattern is defined use it also on clinet
-            if (string.IsNullOrEmpty(scriptPattern))
-            {
-                scriptPattern = this.Pattern;
-            }
-            return new ValidatorMethodData(
-                "pattern",
-                "function(value,element,parameters){var re = new RegExp(/" + scriptPattern + "/); return value.match(re);}",
-                "$.format('" + ErrorMessageFormat + "')"
-            );
-        }
+			if(string.IsNullOrEmpty(scriptPattern))
+				scriptPattern = this.Pattern;
 
-        /// <summary>
-        /// Gets the rule for the given element used by the jQuery validation plugin.
-        /// </summary>
-        public override string GetClientRule(string element)
-        {
-            return "pattern:true";
-        }
+			return new ValidatorMethodData(
+				"pattern" + TypeCount,
+				"function(value,element,parameters){var re = new RegExp(/" + scriptPattern + "/); return value.match(re);}",
+				"$.format('" + ErrorMessageFormat + "')"
+			);
+		}
 
-        /// <summary>
-        /// Gets the message for the given element used by the jQuery validation plugin.
-        /// </summary>
-        public override string GetClientMessage(string element)
-        {
-            return string.Format("pattern:'{0}'", GetLocalizedErrorMessage(element)).Replace("'", "\'");
-        }
+		/// <summary>
+		/// Gets the rule for the given element used by the jQuery validation plugin.
+		/// </summary>
+		public override string GetClientRule(string element)
+		{
+			return string.Format("pattern{0}:true", TypeCount);
+		}
 
-        /// <summary>
-        /// Validates the given element using the Values collection and generates an error if 
-        /// invalid.
-        /// </summary>
-        protected override void Validate(string element)
-        {
-            if (Values.ContainsKey(element) == false || (!ValidateRegex((Values[element] ?? string.Empty).Trim())))
-            {
-                InsertError(element);
-            }
-        }
+		/// <summary>
+		/// Gets the message for the given element used by the jQuery validation plugin.
+		/// </summary>
+		public override string GetClientMessage(string element)
+		{
+			return string.Format("pattern{0}:'{1}'", TypeCount, GetLocalizedErrorMessage(element)).Replace("'", "\'");
+		}
 
-        /// <summary>
-        /// Checks if a value matches defined regular expression
-        /// </summary>
-        private bool ValidateRegex(string value)
-        {
-            Regex pattern = new Regex(this.Pattern);
-            bool match = pattern.IsMatch(value);
+		/// <summary>
+		/// Validates the given element using the Values collection and generates an error if 
+		/// invalid.
+		/// </summary>
+		protected override void Validate(string element)
+		{
+			if(!Values.ContainsKey(element) || (!ValidateRegex((Values[element] ?? string.Empty).Trim())))
+				InsertError(element);
+		}
 
-            return match;
-        }
+		/// <summary>
+		/// Validates the given value against the regular expression.
+		/// </summary>
+		private bool ValidateRegex(string value)
+		{
+			if(string.IsNullOrEmpty(Pattern))
+				return false;
 
-        /// <summary>
-        /// Gets the default error message format in English and is called if no error message is 
-        /// defined in code or in App_GlobalResources.
-        /// </summary>
-        protected override string GetDefaultErrorMessageFormat()
-        {
-            return "The field {0} must match specified Pattern";
-        }
+			return new Regex(Pattern).IsMatch(value);
+		}
 
-    }
+		/// <summary>
+		/// Gets the default error message format in English and is called if no error message is 
+		/// defined in code or in App_GlobalResources.
+		/// </summary>
+		protected override string GetDefaultErrorMessageFormat()
+		{
+			return "The field {0} must match specified Pattern";
+		}
+
+	}
 }

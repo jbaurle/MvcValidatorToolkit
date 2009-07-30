1a8b486d329fe55f081cbd036e5bd53c281dc8d2
jbaurle_cp <SND\jbaurle_cp>
2009-07-30 08:10:51 +0000

    Integrated patch from andysleigh

--- MvcValidatorToolkit/Extensions/NameValueCollectionExtensions.cs
+++ MvcValidatorToolkit/Extensions/NameValueCollectionExtensions.cs
@@ -18,9 +18,11 @@ namespace System.Web.Mvc
 			if(keyToFind == null)
 				throw new ArgumentNullException("keyToFind");
 
+			string k = keyToFind.ToLower().Trim();
+
 			foreach(string key in collection)
 			{
-				if(key.ToLower().Trim().Equals(keyToFind.ToLower().Trim()))
+				if(key != null && key.ToLower().Trim().Equals(k))
 					return true;
 			}
 
